e04e4c23bb41f116304a5fc8daadbb135f9d1829
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-08-24 10:24:02 +0700

    new file rbphotosgalleryvc

---
+++ RBPhotosGallery/RBPhotosGalleryViewController.swift
@@ -0,0 +1,130 @@
+//
+//  RBPhotosGalleryViewController.swift
+//  RBPhotosGallery
+//
+//  Created by Ricki Bin Yamin on 24/08/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBToolkit
+
+public class RBPhotosGalleryViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    var currentPageIndex: Int = 0
+    var hasLayout = false
+    var justMoved = false
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isPagingEnabled = true
+        scrollView.contentMode = .scaleAspectFill
+        scrollView.isDirectionalLockEnabled = true
+        scrollView.backgroundColor = .systemBackground
+        
+        return scrollView
+    }()
+    
+    // MARK: - Public Properties
+    
+    public lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+        activityIndicator.color = .systemGray
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activityIndicator
+    }()
+    
+    // MARK: - Life Cycles
+    
+    public override func loadView() {
+        super.loadView()
+        
+        configureView()
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if !justMoved {
+            activityIndicator.startAnimating()
+            justMoved = false
+        }
+    }
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        justMoved = true
+    }
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        reloadData()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func configureView() {
+        // Default view's background color is system color, it support both dark and light mode to be adaptively changed
+        view.backgroundColor = .systemBackground
+        
+        // Adding all subviews, needs RBToolkit framework
+        view.addAllSubviews(views: [scrollView, activityIndicator])
+        
+        // Configuring autolayout
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func reloadData() {
+            let scrollViewWidth = self.scrollView.frame.width
+            let scrollViewHeight = self.scrollView.frame.height
+                    
+            DispatchQueue.main.async {
+                for view in self.scrollView.subviews {
+                    view.removeFromSuperview()
+                }
+                
+                let processedImages = self.photosGalleryImages()
+                        
+                for i in 0..<processedImages.count {
+                    let imageView = UIImageView(frame: CGRect(x: scrollViewWidth * CGFloat(i), y: 0, width: scrollViewWidth, height: scrollViewHeight))
+                            
+                    imageView.image = processedImages[i]
+                    imageView.contentMode = .scaleAspectFit
+                            
+                    self.scrollView.addSubview(imageView)
+                }
+                        
+                self.scrollView.contentSize = CGSize(width: self.scrollView.frame.width * CGFloat(processedImages.count), height: self.scrollView.frame.height)
+                
+                self.hasLayout = true
+                self.activityIndicator.stopAnimating()
+            }
+        }
+    
+    // MARK: - Public Method
+    
+    public func scrollToPage(page: Int, animated: Bool) {
+        let pageWidth: CGFloat = scrollView.frame.width
+        scrollView.scrollRectToVisible(CGRect(x: pageWidth * CGFloat(page), y: 0, width: pageWidth, height: scrollView.frame.height), animated: animated)
+    }
+    
+    // Should be overrided
+    
+    public func photosGalleryImages() -> [UIImage] {
+        // Run on main thread
+        return []
+    }
+}
