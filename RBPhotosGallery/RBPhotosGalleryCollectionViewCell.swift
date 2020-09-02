4a8f3df96a38d610f3555489d26bb7053e4fbae9
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-09-02 16:05:29 +0700

    scrollview

--- RBPhotosGallery/RBPhotosGalleryCollectionViewCell.swift
+++ RBPhotosGallery/RBPhotosGalleryCollectionViewCell.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class RBPhotosGalleryCollectionViewCell: UICollectionViewCell {
 	
+	lazy var scrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		
+		return scrollView
+	}()
+	
 	lazy var imageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -17,4 +24,27 @@ class RBPhotosGalleryCollectionViewCell: UICollectionViewCell {
 		
 		return imageView
 	}()
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		contentView.addSubview(scrollView)
+		scrollView.addSubview(imageView)
+		
+		NSLayoutConstraint.activate([
+			scrollView.topAnchor.constraint(equalTo: contentView.topAnchor),
+			scrollView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+			scrollView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+			scrollView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+			
+			imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+			imageView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+			imageView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+			imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
+		])
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 }
