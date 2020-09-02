0a4ec1456f495d33f9720dd82358fb941513322c
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-09-02 15:32:22 +0700

    adding collection view

--- RBPhotosGallery/RBPhotosGalleryViewController.swift
+++ RBPhotosGallery/RBPhotosGalleryViewController.swift
@@ -12,11 +12,44 @@ open class RBPhotosGalleryViewController: UIViewController {
 	
 	// MARK: - Private Properties
 	
+	private lazy var collectionViewFlowLayout: UICollectionViewFlowLayout = {
+		let layout = UICollectionViewFlowLayout()
+		layout.scrollDirection = .horizontal
+		
+		return layout
+	}()
+	
+	private lazy var collectionView: UICollectionView = {
+		let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+		collectionView.backgroundColor = .systemBackground
+		collectionView.showsHorizontalScrollIndicator = false
+		
+		return collectionView
+	}()
+	
 	// MARK: - Public Properties
 	
 	// MARK: - Lifecycle
 	
+	open override func loadView() {
+		super.loadView()
+		
+		configureView()
+	}
+	
 	// MARK: - Private Method
+	
+	private func configureView() {
+		view.addSubview(collectionView)
+		
+		NSLayoutConstraint.activate([
+			collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+			collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+			collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+			collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+		])
+	}
 	
 	// MARK: - Public Method
 }
---
+++ RBPhotosGalleryExample/TryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TryViewController.swift
+//  RBPhotosGalleryExample
+//
+//  Created by Ricki Bin Yamin on 02/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBPhotosGallery
+
+class TryViewController: RBPhotosGalleryViewController {
+	
+}
